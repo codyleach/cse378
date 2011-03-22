e132f98d5748ae49c3100673c9ac2223e4f37c95
codyleach <codyleach@gmail.com>
2011-03-21 23:26:21 -0500

    Adding things to the git

---
+++ src/com/communitymarket/home.java
@@ -0,0 +1,14 @@
+package com.communitymarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class home extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

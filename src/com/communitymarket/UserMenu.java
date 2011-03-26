e4bf03bda5b37cc9ea682c1bca22ddb81fa89ff1
Dan Sveen <dsveen@gmail.com>
2011-03-26 17:18:43 -0500

    Committing Buttons

--- src/com/communitymarket/UserMenu.java
+++ src/com/communitymarket/UserMenu.java
@@ -1,9 +1,10 @@
 package com.communitymarket;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
 
 public class UserMenu extends Activity {
 	/** Called when the activity is first created. */
@@ -13,5 +14,16 @@ public class UserMenu extends Activity {
         setContentView(R.layout.menu);
         
         // Get the menu items
+        
+      //Producer Button
+        final Button farmersButton = (Button) findViewById(R.id.farmers_button);
+        farmersButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Intent intent = new Intent(UserMenu.this, farmersList.class);
+				startActivityForResult(intent, 0);
+            	
+            }
+        });
     }
 }

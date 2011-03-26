b0b33f598618dc5260ab57a4122cb1542a630858
codyleach <codyleach@gmail.com>
2011-03-26 18:30:25 -0500

    Fixed Dan's stupid mistake

--- src/com/communitymarket/UserMenu.java
+++ src/com/communitymarket/UserMenu.java
@@ -7,26 +7,27 @@ import android.view.View;
 import android.widget.Button;
 
 public class UserMenu extends Activity {
-	private UserType userType;
+	//private UserType userType;
 	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       
+        setContentView(R.layout.menu);
         
         // Get the type of user
-        userType = (UserType) getIntent().getExtras().getSerializable("usertype");
+        //userType = (UserType) getIntent().getExtras().getSerializable("usertype");
         
         // Producer Button
         final Button farmersButton = (Button) findViewById(R.id.farmers_button);
-        farmersButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-            	Intent intent = new Intent(UserMenu.this, farmersList.class);
-				startActivityForResult(intent, 0);
-            	
-            }
-        });
+        if (farmersButton != null) {
+        	farmersButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // Perform action on click
+                	Intent intent = new Intent(UserMenu.this, farmersList.class);
+    				startActivityForResult(intent, 0);
+                }
+            });
+        }
     }
 }

6a2b8613aa4a2eb1c724da39971430f78912967c
Diego Moreno <diegomoreno@Diego-MBP.local>
2011-03-27 13:37:42 -0500

    new

--- src/com/communitymarket/marketSearchResults.java
+++ src/com/communitymarket/marketSearchResults.java
@@ -1,7 +1,10 @@
 package com.communitymarket;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class marketSearchResults extends Activity {
 	
@@ -11,6 +14,17 @@ public class marketSearchResults extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.market_search_results); 
         
+      //Get Info Button
+        final Button infoButton = (Button) findViewById(R.id.getInfo);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Intent intent = new Intent(marketSearchResults.this, marketInformation.class);
+            	intent.putExtra("usertype", UserType.Consumer);
+				startActivityForResult(intent, 0);
+            	
+            }
+        });
         
     }
 }

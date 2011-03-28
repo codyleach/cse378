2affbd6afdf28320317a3b6f279013e32a98fb17
codyleach <codyleach@gmail.com>
2011-03-27 22:05:34 -0500

    working at on Farmer page

--- src/com/communitymarket/farmersList.java
+++ src/com/communitymarket/farmersList.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class farmersList extends Activity {
 	
@@ -13,9 +15,19 @@ public class farmersList extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.farmers_list); 
+        setContentView(R.layout.farmers_list);
         
-        
+        // Search Button
+        Button searchButton = (Button) findViewById(R.id.search_farmers_button);
+        if (searchButton != null) {
+        	searchButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // Perform action on click
+                	Intent intent = new Intent(farmersList.this, Farmer.class);
+    				startActivityForResult(intent, 0);
+                }
+            });
+        }
     }
     
     @Override

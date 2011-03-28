f6d52a9d4022c3f573f567bf556e7f6f13394c6e
codyleach <codyleach@gmail.com>
2011-03-27 19:08:56 -0500

    Working on a common header file

--- src/com/communitymarket/UserMenu.java
+++ src/com/communitymarket/UserMenu.java
@@ -2,12 +2,13 @@ package com.communitymarket;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class UserMenu extends Activity {
-	//private UserType userType;
+	private UserType userType;
 	
 	/** Called when the activity is first created. */
     @Override
@@ -16,7 +17,16 @@ public class UserMenu extends Activity {
         setContentView(R.layout.menu);
         
         // Get the type of user
-        //userType = (UserType) getIntent().getExtras().getSerializable("usertype");
+        userType = (UserType) getIntent().getExtras().getSerializable("usertype");
+        
+        // Show administrative options?
+        if (userType == UserType.Coordinator) {
+        	// Get the manage markets button
+        	Button manageButton = (Button) findViewById(R.id.manage_button);
+        	if (manageButton != null) {
+        		manageButton.setVisibility(View.VISIBLE);
+        	}
+        }
         
         // Producer Button
         final Button farmersButton = (Button) findViewById(R.id.farmers_button);
